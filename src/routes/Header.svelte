8e1bb2894808294ca8a354235b680d27422dac35
DanR <daniel_ramsden@proton.me>
2025-10-06 15:38:03 +0100

    update header to fix routing page/sections and add test page

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,6 +1,8 @@
 
 <script lang="ts">
 	import logo from '$lib/assets/logo.png';
+
+
 </script>
 
 <header>
@@ -9,14 +11,16 @@
             <div class="flex justify-between items-center h-20">
                 <div class="hidden lg:flex items-center space-x-15">
                     <img class= "h-40" src={logo} alt="logo">
-                    <a href="#who-i-am" class="text-sm font-medium text-gray-700 hover:text-green-600 transition">Who I Am</a>
-                    <a href="#what-i-treat" class="text-sm font-medium text-gray-700 hover:text-green-600 transition">What I Treat</a>
-                    <a href="#my-approach" class="text-sm font-medium text-gray-700 hover:text-green-600 transition">My Approach</a>
-                    <a href="#services" class="text-sm font-medium text-gray-700 hover:text-green-600 transition">Services</a>
-                    <a href="#faqs" class="text-sm font-medium text-gray-700 hover:text-green-600 transition">FAQs</a>
-                    <a href="#blog" class="text-sm font-medium text-gray-700 hover:text-green-600 transition">Blog</a>
-                    <a href="#socials" class="text-sm font-medium text-gray-700 hover:text-green-600 transition">Socials</a>
-                    <a href="#book" class="ml-4 bg-gradient-to-r from-green-400 to-green-500 text-white px-6 py-2.5 rounded-full font-medium hover:shadow-lg hover:scale-105 transition-all">Book Now</a>
+                    <a href="/#who-i-am" class="text-sm font-medium text-gray-700 hover:text-green-600 transition">Who I Am</a>
+                    <a href="/#what-i-treat" class="text-sm font-medium text-gray-700 hover:text-green-600 transition">What I Treat</a>
+                    <a href="/#my-approach" class="text-sm font-medium text-gray-700 hover:text-green-600 transition">My Approach</a>
+                    <a href="/#services" class="text-sm font-medium text-gray-700 hover:text-green-600 transition">Services</a>
+                    <a href="/#faqs" class="text-sm font-medium text-gray-700 hover:text-green-600 transition">FAQs</a>
+                    <a href="/#blog" class="text-sm font-medium text-gray-700 hover:text-green-600 transition">Blog</a>
+                    <a href="/#socials" class="text-sm font-medium text-gray-700 hover:text-green-600 transition">Socials</a>
+                    <a href="/who-i-am" class="text-sm font-medium text-gray-700 hover:text-green-600 transition">TEST</a>
+                    <a href="/#book" class="ml-4 bg-gradient-to-r from-green-400 to-green-500 text-white px-6 py-2.5 rounded-full font-medium hover:shadow-lg hover:scale-105 transition-all">Book Now</a>
+                    
                 </div>
             </div>
         </div>
